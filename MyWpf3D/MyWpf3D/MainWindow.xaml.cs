8ce112f1d1ce5c1215300106afa665ad6b1bc318
Alfred Manoj <37899575+AlfredManoj@users.noreply.github.com>
2018-04-24 09:37:24 +0300

    WPF 3D
    This solution contains wpf 3d samples and the commonly used styles and transformations for wpf 3d

---
+++ MyWpf3D/MyWpf3D/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace MyWpf3D
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ScaleTransform3D myRotateTransform3D = new ScaleTransform3D();
+            myRotateTransform3D.ScaleX = Convert.ToDouble(tbScaleX.Text);
+            myRotateTransform3D.ScaleY = Convert.ToDouble(tbScaleY.Text);
+            myRotateTransform3D.ScaleZ = Convert.ToDouble(tbScaleZ.Text);
+            
+            //Transform3DGroup myTransform3DGroup = new Transform3DGroup();
+            //myTransform3DGroup.Children.Add(myRotateTransform3D);
+            mygeo.Transform = myRotateTransform3D;
+        }
+    }
+}
