2bf82e423fd7fb07f243b6a6dc5216cba8916406
Alfred Manoj <37899575+AlfredManoj@users.noreply.github.com>
2018-04-24 09:34:03 +0300

    Animated Expander Control WPF
    This solution contains the animated expander control for WPF

---
+++ SampleAnimationExpander/SampleAnimationPopup/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleAnimationPopup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exemployer_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exgeneral_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exemployee_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exsalary_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exleave_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exattendance_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
